a7863bc4316d4ea2b7dd1882f0c5d5b4123651c2
Todd <github@quaddi.com>
2023-09-01 09:11:14 -0700

    refresh resources per user instead of stored token

--- internal/daemon/cache/repository_refresh.go
+++ internal/daemon/cache/repository_refresh.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/boundary/api/targets"
 	"github.com/hashicorp/boundary/internal/errors"
 	"github.com/hashicorp/boundary/internal/observability/event"
+	"golang.org/x/exp/slices"
 )
 
 type targetRetrievalFunc func(ctx context.Context, keyringstring, tokenName string) ([]*targets.Target, error)
@@ -49,24 +50,45 @@ func (r *Repository) Refresh(ctx context.Context, opt ...Option) error {
 		return errors.Wrap(ctx, err, op)
 	}
 
-	// TODO: Only refresh once per user instead of once per token
-	var retErr error
+	// build the users from the retrieved stored tokens
+	users := make(map[User][]*storedToken)
 	for _, st := range storedTokens {
-		at := r.tokenLookupFn(st.KeyringType, st.TokenName)
-		if at == nil {
-			if err := r.deleteStoredToken(ctx, st); err != nil {
-				retErr = stderrors.Join(retErr, err)
+		key := User{UserId: st.UserId, BoundaryAddr: st.BoundaryAddr}
+		users[key] = append(users[key], st)
+	}
+
+	var retErr error
+	for u, tokens := range users {
+		// sort the tokens so the most recently accessed is first
+		slices.SortFunc(tokens, func(a, b *storedToken) int {
+			return a.LastAccessedTime.Compare(b.LastAccessedTime)
+		})
+		slices.Reverse(tokens)
+
+		var lookupErr error
+		var retrievedTargets []*targets.Target
+		for _, t := range tokens {
+			lookupErr = nil
+
+			at := r.tokenLookupFn(t.KeyringType, t.TokenName)
+			if at == nil {
+				if err := r.deleteStoredToken(ctx, t); err != nil {
+					lookupErr = err
+				}
+				continue
 			}
-			continue
+
+			tars, err := opts.withTargetRetrievalFunc(ctx, u.BoundaryAddr, at.Token)
+			if err != nil {
+				lookupErr = errors.Wrap(ctx, err, op, errors.WithMsg("for user %v using stored token %#v", u, t))
+				continue
+			}
+			retrievedTargets = tars
 		}
-		u := &User{UserId: st.UserId, BoundaryAddr: st.BoundaryAddr}
-		t, err := opts.withTargetRetrievalFunc(ctx, st.BoundaryAddr, at.Token)
-		if err != nil {
-			retErr = stderrors.Join(retErr, errors.Wrap(ctx, err, op, errors.WithMsg("for user %v", u)))
-			continue
-		}
-		event.WriteSysEvent(ctx, op, fmt.Sprintf("updating %d targets for user %v", len(t), u))
-		if err := r.refreshTargets(ctx, u, t); err != nil {
+		retErr = stderrors.Join(retErr, lookupErr)
+
+		event.WriteSysEvent(ctx, op, fmt.Sprintf("updating %d targets for user %v", len(retrievedTargets), u))
+		if err := r.refreshTargets(ctx, &u, retrievedTargets); err != nil {
 			retErr = stderrors.Join(retErr, errors.Wrap(ctx, err, op, errors.WithMsg("for user %v", u)))
 			continue
 		}
