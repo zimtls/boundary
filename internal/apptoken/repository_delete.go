61853e9f048fab29fafe4b5bd9bceaa40c85e2cd
cameronperera <cameron.perera@hashicorp.com>
2023-12-12 22:04:02 -0600

    feat (apptoken): finish repository delete

--- internal/apptoken/repository_delete.go
+++ internal/apptoken/repository_delete.go
@@ -5,6 +5,7 @@ package apptoken
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/boundary/internal/db"
 	"github.com/hashicorp/boundary/internal/errors"
@@ -32,4 +33,29 @@ func (r *Repository) DeleteAppToken(ctx context.Context, publicId string, _ ...O
 	if err != nil {
 		return db.NoRowsAffected, errors.Wrap(ctx, err, op, errors.WithMsg("unable to get oplog wrapper"))
 	}
+	var rowsDeleted int
+	_, err = r.writer.DoTx(
+		ctx,
+		db.StdRetryCnt,
+		db.ExpBackoff{},
+		func(_ db.Reader, w db.Writer) error {
+			cp := at.clone()
+			md, err := cp.oplog(ctx, oplog.OpType_OP_TYPE_DELETE)
+			if err != nil {
+				return errors.Wrap(ctx, err, op, errors.WithMsg("unable to generate oplog metadata"))
+			}
+			rowsDeleted, err = w.Delete(ctx, cp, db.WithOplog(oplogWrapper, md))
+			if err != nil {
+				return err
+			}
+			if rowsDeleted > 1 {
+				return errors.New(ctx, errors.MultipleRecords, op, "more than 1 app token would have been deleted")
+			}
+			return nil
+		},
+	)
+	if err != nil {
+		return db.NoRowsAffected, errors.Wrap(ctx, err, op, errors.WithMsg(fmt.Sprintf("unable to delete %s", publicId)))
+	}
+	return rowsDeleted, nil
 }
