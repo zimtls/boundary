20e59a0e82cf45569cfbee6778844b801540bc21
Michael Milton <michael.milton@hashicorp.com>
2023-12-18 16:38:11 -0500

    tweak tests and move sessions to prior established file

--- internal/db/schema/migrations/oss/postgres/80/05_session_base_table_updates.up.sql
+++ internal/db/schema/migrations/oss/postgres/80/05_session_base_table_updates.up.sql
@@ -3,11 +3,19 @@
 
 begin;
 
-  -- Add new indexes for the create time and update time queries.
-  create index session_create_time_public_id_idx
-      on session (create_time desc, public_id asc);
-  create index session_update_time_public_id_idx
-      on session (update_time desc, public_id asc);
+  -- Add new indexes for the create and update time queries.
+  create index session_create_time_list_idx
+            on session (create_time desc,
+                        public_id,
+                        project_id,
+                        user_id,
+                        termination_reason);
+  create index session_update_time_list_idx
+            on session (update_time desc,
+                        public_id,
+                        project_id,
+                        user_id,
+                        termination_reason);
 
   analyze session;
 
--- internal/db/sqltest/tests/pagination/session.sql
+++ internal/db/sqltest/tests/pagination/session.sql
@@ -4,8 +4,8 @@
 begin;
   select plan(2);
 
-  select has_index('session', 'session_create_time_public_id_idx', array['create_time', 'public_id']);
-  select has_index('session', 'session_update_time_public_id_idx', array['update_time', 'public_id']);
+  select has_index('session', 'session_create_time_list_idx', array['create_time', 'public_id', 'project_id', 'user_id', 'termination_reason']);
+  select has_index('session', 'session_update_time_list_idx', array['update_time', 'public_id', 'project_id', 'user_id', 'termination_reason']);
 
   select * from finish();
 
